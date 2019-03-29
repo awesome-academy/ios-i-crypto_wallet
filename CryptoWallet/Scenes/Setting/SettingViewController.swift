f5aae47e54b29a862190c80d2fadc920439e6e72
Phan Dinh Van <phan.dinh.van@framgia.com>
2019-03-29 08:57:49 +0700

    task 9344: add tabbar

---
+++ CryptoWallet/Scenes/Setting/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  CryptoWallet
+//
+//  Created by Phan Dinh Van on 3/29/19.
+//  Copyright © 2019 Phan Dinh Van. All rights reserved.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
